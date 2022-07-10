5b723f220d4b03cb8f1f5b67d7b1d4065d81f7f3
thugiang <xanhtham.cuc@gmail.com>
2022-07-10 15:24:24 +1200

    Multiple pointers

--- script.js
+++ script.js
@@ -143,3 +143,27 @@ function sumZero(arr) {
 }
 
 console.log(sumZero([-3, -4, -2, -1, 0, 1, 5]));
+
+//Challenge: contUniqueValues. Implement a function called contUniqueValues,
+//which accepts a sorted array, and counts the unique values in the array.
+//There can be negative numbers in the array, but it will always be sorted.
+//ex: countUniqueValues([1,1,1,1,1,2]) //2
+//countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]) //7
+
+function countUniqueValues(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  var i = 0;
+  for (var j = 1; j < arr.length; j++){
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1; //(i length)
+}
+
+console.log(countUniqueValues([1,2,2,3,4]))
+
+//if arr[i] !== arr[j] -> i++ (move up) & arr[i] = arr[j]
